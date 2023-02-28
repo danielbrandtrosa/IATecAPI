875853e247c7bd924135244cb582dfba51540a9e
Daniel Brandt Rosa <daniel_odan@hotmail.com>
2023-02-28 15:19:47 -0300

    pastas e enum

---
+++ IATecAPI/Enums/StatusVenda.cs
@@ -0,0 +1,13 @@
+﻿namespace IATecAPI.Enums
+{
+
+    public enum StatusVenda
+    {
+        //[Description("AguardandoPagamento")]
+        AguardandoPagamento = 1,
+        PagamentoAprovado = 2,
+        Cancelada = 3,
+        Enviado = 4,
+        Entregue = 5,
+    }
+}
